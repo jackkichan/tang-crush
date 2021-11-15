c4d779575b12974038ffbc047f854e1a28ff956c
jackkichan <93869193+jackkichan@users.noreply.github.com>
2021-11-15 22:14:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'nhắn luôn cho mình nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://Facebook.com//m.me/100014996986769
+    messLink: 'https://m.me/t/100014996986769
 }
